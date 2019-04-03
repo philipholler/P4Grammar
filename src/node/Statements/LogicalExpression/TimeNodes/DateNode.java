ae6f0662c4ea648cafc0760bdf9e8ec05f8887bf
Philip Holler <pholle17@student.aau.dk>
2019-04-03 13:54:19 +0200

    DateNode now works.

--- src/node/Statements/LogicalExpression/TimeNodes/DateNode.java
+++ src/node/Statements/LogicalExpression/TimeNodes/DateNode.java
@@ -4,6 +4,21 @@ import node.Statements.LogicalExpression.LogicalExprNode;
 import utils.StringUtils;
 
 public class DateNode extends LogicalExprNode {
+    // todo: find proper date library and implement - Philip
+    private int day;
+    private int month;
+    private int year;
+
+    public DateNode(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public DateNode(int day, int month) {
+        this.day = day;
+        this.month = month;
+    }
 
     @Override
     public String getTreeString(int indentation) {
@@ -13,6 +28,10 @@ public class DateNode extends LogicalExprNode {
 
     @Override
     public String toString() {
-        return "DateNode()";
+        return "DateNode(" +
+                "day=" + day +
+                "/" + month +
+                "/" + year +
+                ')';
     }
 }
