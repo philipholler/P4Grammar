15b3a63beefd52e577e59c2e2c49f5b2bae3b37c
Magnus <magnjensen@gmail.com>
2019-04-16 22:20:01 +0200

    Timed events now work for EventEveryNodes.

--- src/main/java/codegen/default_classes/event/TimeEventManager.java
+++ src/main/java/codegen/default_classes/event/TimeEventManager.java
@@ -4,6 +4,7 @@ import codegen.default_classes.Server;
 import codegen.default_classes.SignalData;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -19,7 +20,24 @@ public class TimeEventManager extends Thread {
 
     @Override
     public void run(){
+        while (true){
+            LocalDateTime now = LocalDateTime.now();
+            TimeEvent nextEvent = eventExecutions.first();
 
+            if(now.compareTo(nextEvent.getNextExecutionTime()) < 0){
+                Long timeDifference = ChronoUnit.MILLIS.between(now, eventExecutions.first().getNextExecutionTime());
+                try {
+                    Thread.sleep(timeDifference);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            nextEvent.executeEventThread();
+            eventExecutions.remove(nextEvent);
+            nextEvent.rescheduleEvent();
+            eventExecutions.add(nextEvent);
+        }
     }
 
 
