6b2ca18b83c65a5c32e7b1d0ce173f87b77161a1
Philip Holler <pholle17@student.aau.dk>
2019-04-23 13:24:16 +0200

    Generate code is now it's own function in Compiler. Changed to use the ast.accept instead of visitor.visit. This ensures, that the AST is only visited by the correct visitors i.e. visitor extending the ASTBaseVisitor.

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -38,8 +38,10 @@ public class Compiler {
             // Visit with AstBuilderVisitor to create ast
             AstBuilderVisitor astVisitor = new AstBuilderVisitor();
             Node ast = astVisitor.visit(tree);
+
             // Print ast. Use the Node.getTreeString() to pretty-print the AST.
             System.out.println(ast.getTreeString(0));
+
             // Decorate AST using all visitors
             decorateAST(ast);
 
@@ -52,17 +54,21 @@ public class Compiler {
             // Print new AST
             System.out.println(ast.getTreeString(0));
 
-            // Generate java classes corresponding to the (device and signal) type definitions// Todo move to seperate function
-            ClassGenerationVisitor classGenVisitor = new ClassGenerationVisitor();
-            classGenVisitor.visit(ast);
-            MainGenerationVisitor mainGenVisitor = new MainGenerationVisitor();
-            mainGenVisitor.visit(ast);
-            EventInitializationVisitor eventInitGen = new EventInitializationVisitor();
-            eventInitGen.visit(ast);
+            // Generate code
+            generateJavaCode(ast);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void generateJavaCode(Node ast){
+        ast.accept(new ClassGenerationVisitor());
+
+        ast.accept(new MainGenerationVisitor());
+
+        ast.accept(new EventInitializationVisitor());
     }
 
     private static void decorateAST(Node ast){
@@ -78,6 +84,8 @@ public class Compiler {
     private static void optimiseExpr(Node ast){
         ast.accept(new OptimiseExprVisitor());
     }
+
+
     private static void generateByteCode(Node ast){
         JasminVisitor jv = new JasminVisitor();
         //System.out.println(jv.visit(ast));
